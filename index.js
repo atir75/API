b4d27764e7ca85a4889db3c4c8f61f9d407ddfa4
Maria Rita Mendonça <mariamendonca.gbmtech@outlook.com.br>
2023-01-26 20:36:47 -0300

    commit initial

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require(`express`)
 const morgan = require(`morgan`)
 const cors = require(`cors`)
-const bodyParser = require (`bodyParser`) //chamada dos pacotes e atribuindo as var, as variaveis serão do tipo do meu pacote 
+const bodyParser = require (`body-Parser`) //chamada dos pacotes e atribuindo as var, as variaveis serão do tipo do meu pacote 
 
 const app = express()
 
 app.use(morgan(`dev`))
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
+app.listen(21262, () =>{
+    console.log(`Express started at http://localhost:21262`)
+})
