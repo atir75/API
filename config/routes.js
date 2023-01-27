3e26aa18183c0cc01de840eb2b755d08d2043f2c
Maria Rita Mendonça <mariamendonca.gbmtech@outlook.com.br>
2023-01-26 21:26:37 -0300

    commit organization

---
+++ config/routes.js
@@ -0,0 +1,47 @@
+const express = require(`express`)
+const routes = express.Router()
+
+
+
+let db = [
+    { '1': {Nome: `Cliente 1`, Idade: `20`} },
+    { '2': {Nome: `Cliente 2`, Idade: `20`} },
+    { '3': {Nome: `Cliente 3`, Idade: `20`} }
+]
+
+//buscar dados 
+routes.get(`/`, (req, res) => {
+    return res.json({db})
+})
+
+
+//inserir dados 
+routes.post(`/`, (req, res) => {
+    const body = req.body
+
+    if(!body)
+    return res.status(400).end()
+
+    db.push(body)
+    return res.json(body)
+})
+
+
+routes.delete(`/:id`, (req, res) => {
+    const id = req.params.id
+
+    let newDB = db.filter(item =>{ //pegando o banco e filtrando para retornar onde o id for diferente do parametro
+        if(!item[id])
+        return item
+    })
+    db = newDB
+
+    return res.send(newDB)
+})
+
+
+
+module.exports = routes
+
+
+
