9363a83934fa26f7a48cd067ea9ce0f650d55ea3
Maria Rita Mendonça <mariamendonca.gbmtech@outlook.com.br>
2023-01-26 21:26:24 -0300

    final commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require(`express`)
 const morgan = require(`morgan`)
 const cors = require(`cors`)
 const bodyParser = require (`body-Parser`) //chamada dos pacotes e atribuindo as var, as variaveis serão do tipo do meu pacote 
+const routes = require(`./config/routes`)
+
 
 const app = express()
 
@@ -9,33 +11,7 @@ app.use(morgan(`dev`))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
-
-
-let db = [
-    { '1': {Nome: `Cliente 1`, Idade: `20`} },
-    { '2': {Nome: `Cliente 2`, Idade: `20`} },
-    { '3': {Nome: `Cliente 3`, Idade: `20`} }
-]
-
-//buscar dados 
-app.get(`/`, (req, res) => {
-    return res.json({db})
-})
-
-
-//inserir dados 
-app.post(`/`, (req, res) => {
-    const body = req.body
-
-    if(!body)
-    return res.status(400).end()
-
-    db.push(body)
-    return res.json(body)
-})
-
-
-
+app.use(routes)
 
 
 app.listen(21262, () =>{
